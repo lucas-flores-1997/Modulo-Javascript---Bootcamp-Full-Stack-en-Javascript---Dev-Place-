df492f1677ebe77a2a212610f4b016ac0ae1c8aa
Lucas Flores <55467148+lucas-flores-1997@users.noreply.github.com>
2022-10-25 17:17:04 -0300

    Add files via upload

--- pair programming 7/main.js
+++ pair programming 7/main.js
@@ -1,5 +1,4 @@
 const prompt = require("prompt-sync")({ sigint: true});
-let agenda = [];
 
 const cargarContacto =()=> {
     let nuevoContacto = [];
@@ -9,20 +8,47 @@ const cargarContacto =()=> {
     nuevoContacto [3] = prompt('Ingrese el telefono: ')
     return nuevoContacto;
 }
-agenda = [['39222111','Ema','Fernandez','2235555555'],
+var agenda = [['39222111','Ema','Fernandez','2235555555'],
 ['40223311','Carlos','Perez','2234455444'],
 ['402323212','Lucas','Gimenez','2235555555']
 ];
 
 agenda.unshift(cargarContacto());
 agenda.push(cargarContacto());
-console.log(agenda);
+
+console.table(agenda);
 
 agenda.splice(prompt("eliminar contacto de la posicion: "),1);
 
-console.log(agenda);
+console.table(agenda);
 
-//agenda.splice(indice,1);
+
+function buscarArray(dni){
+    for (let index = 0; index < agenda.length; index++) {
+        let dniDatos = agenda[index][0];
+        if (dniDatos == dni){
+            return index;
+        }    
+    }
+}
+
+agenda.splice(buscarArray(prompt("ingrese el dni del contacto que quiere borrar: ")),1);
+console.table(agenda);
+
+
+
+
+
+
+/*
+/*function eliminarIndice(dni){
+    let contactoAEliminar = agenda.forEach( (ele) => ele[0] = dni );
+    let indiceAEliminar = agenda.indexOf(contactoAEliminar);
+    agenda.splice(indiceAEliminar, 1);
+    console.log('Se ha eliminado el contacto.');
+}
+
+console.log(agenda);
 /*
 function buscarIndice (agenda){
     let contador = 0;
